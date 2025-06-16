98288bb63f2a4adaffe47bf3d8081d70379d4ba5
Bo <bojanradovski@gmail.com>
2025-06-16 17:55:35 +0200

    SOAP chatrestrict functionality added

--- fit4life/src/main/java/com/example/fit4life/soap/endpoint/UserEndpoint.java
+++ fit4life/src/main/java/com/example/fit4life/soap/endpoint/UserEndpoint.java
@@ -1,6 +1,8 @@
 package com.example.fit4life.soap.endpoint;
 import org.proekt2.fit4life.BanUserRequest;
 import org.proekt2.fit4life.BanUserResponse;
+import org.proekt2.fit4life.ChatRestrictUserRequest;
+import org.proekt2.fit4life.ChatRestrictUserResponse;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
@@ -13,7 +15,7 @@ public class UserEndpoint {
     public BanUserResponse handleBanUser(@RequestPayload BanUserRequest request){
         BanUserResponse response = new BanUserResponse();
 
-        if (request.isBan()){
+        if (request.isBanned()){
             response.setStatus("SUCCESS");
             response.setMessage("User " + request.getUsername() + " has been banned.");
         }else{
@@ -21,6 +23,21 @@ public class UserEndpoint {
             response.setMessage("Ban flag was false - no action taken.");
         }
         
+        return response;
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart="ChatRestrictUserRequest")
+    @ResponsePayload
+    public ChatRestrictUserResponse handleChatRestrictUser(@RequestPayload ChatRestrictUserRequest request){
+        ChatRestrictUserResponse response = new ChatRestrictUserResponse();
+
+        if(request.isChatRestricted()){
+            response.setStatus("SUCCESS");
+            response.setMessage("User " + request.getUsername() + " has been chatrestricted.");
+        }else{
+            response.setStatus("NO_ACTION");
+            response.setMessage("ChatRestrict flag was false - no action taken.");
+        }
         return response;
     }
 }
