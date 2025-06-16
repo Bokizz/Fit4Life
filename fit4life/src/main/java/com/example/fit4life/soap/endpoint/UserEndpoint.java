a5546f348aed01e1f35599a08de0590431b427e1
Bo <bojanradovski@gmail.com>
2025-06-16 18:42:58 +0200

    SOAP resetPassword functionality added

--- fit4life/src/main/java/com/example/fit4life/soap/endpoint/UserEndpoint.java
+++ fit4life/src/main/java/com/example/fit4life/soap/endpoint/UserEndpoint.java
@@ -1,27 +1,43 @@
 package com.example.fit4life.soap.endpoint;
+import java.util.Optional;
+
 import org.proekt2.fit4life.BanUserRequest;
 import org.proekt2.fit4life.BanUserResponse;
 import org.proekt2.fit4life.ChatRestrictUserRequest;
 import org.proekt2.fit4life.ChatRestrictUserResponse;
+import org.proekt2.fit4life.ResetPasswordRequest;
+import org.proekt2.fit4life.ResetPasswordResponse;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import com.example.fit4life.model.User;
+import com.example.fit4life.repository.UserRepository;
 @Endpoint
 public class UserEndpoint {
     private static final String NAMESPACE_URI = "http://fit4life.com/fit4life/usersoap";
+    private UserRepository userRepository;
     @PayloadRoot(namespace = NAMESPACE_URI, localPart="BanUserRequest")
     @ResponsePayload
     public BanUserResponse handleBanUser(@RequestPayload BanUserRequest request){
         BanUserResponse response = new BanUserResponse();
 
-        if (request.isBanned()){
-            response.setStatus("SUCCESS");
-            response.setMessage("User " + request.getUsername() + " has been banned.");
-        }else{
-            response.setStatus("NO_ACTION");
-            response.setMessage("Ban flag was false - no action taken.");
+        Optional<User> userOpt = userRepository.findByUsername(request.getUsername());
+        if(userOpt.isPresent()){
+            if (request.isBanned()){
+                User user =userOpt.get();
+                user.setBanned(true);
+                userRepository.save(user);
+
+                response.setStatus("SUCCESS");
+                response.setMessage("User " + request.getUsername() + " has been banned.");
+            }else{
+                response.setStatus("NO_ACTION");
+                response.setMessage("Ban flag was false - no action taken.");
+            }
         }
+        
         
         return response;
     }
@@ -31,12 +47,39 @@ public class UserEndpoint {
     public ChatRestrictUserResponse handleChatRestrictUser(@RequestPayload ChatRestrictUserRequest request){
         ChatRestrictUserResponse response = new ChatRestrictUserResponse();
 
-        if(request.isChatRestricted()){
+        Optional<User> userOpt = userRepository.findByUsername(request.getUsername());
+        if(userOpt.isPresent()){
+            if(request.isChatRestricted()){
+                User user = userOpt.get();
+                user.setChatRestricted(true);
+                userRepository.save(user);
+                
+                response.setStatus("SUCCESS");
+                response.setMessage("User " + request.getUsername() + " has been chatrestricted.");
+            }else{
+                response.setStatus("NO_ACTION");
+                response.setMessage("ChatRestrict flag was false - no action taken.");
+            }
+        }
+        return response;
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "ResetPasswordRequest")
+    @ResponsePayload
+    public ResetPasswordResponse handleResetPassword(@RequestPayload ResetPasswordRequest request){
+        ResetPasswordResponse response = new ResetPasswordResponse();
+
+        Optional<User> userOpt = userRepository.findByUsername(request.getUsername());
+        if(userOpt.isPresent()){
+            User user = userOpt.get();
+            user.setPassword(request.getNewPassword());
+            userRepository.save(user);
+
             response.setStatus("SUCCESS");
-            response.setMessage("User " + request.getUsername() + " has been chatrestricted.");
-        }else{
+            response.setMessage("Password successfully updated for user:"+user.getUsername());
+        } else{
             response.setStatus("NO_ACTION");
-            response.setMessage("ChatRestrict flag was false - no action taken.");
+            response.setMessage("User not found!");
         }
         return response;
     }
