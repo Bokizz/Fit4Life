c56778d6818437599dd9ed85ba0b3df1f86e184c
Bo <bojanradovski@gmail.com>
2025-06-16 19:07:19 +0200

    SOAP subscription functionality added

--- fit4life/src/main/java/com/example/fit4life/soap/endpoint/UserEndpoint.java
+++ fit4life/src/main/java/com/example/fit4life/soap/endpoint/UserEndpoint.java
@@ -7,17 +7,23 @@ import org.proekt2.fit4life.ChatRestrictUserRequest;
 import org.proekt2.fit4life.ChatRestrictUserResponse;
 import org.proekt2.fit4life.ResetPasswordRequest;
 import org.proekt2.fit4life.ResetPasswordResponse;
+import org.proekt2.fit4life.SubscribeStudioRequest;
+import org.proekt2.fit4life.SubscribeStudioResponse;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
+import com.example.fit4life.model.Studio;
+import com.example.fit4life.model.Subscription;
 import com.example.fit4life.model.User;
+import com.example.fit4life.repository.StudioRepository;
 import com.example.fit4life.repository.UserRepository;
 @Endpoint
 public class UserEndpoint {
     private static final String NAMESPACE_URI = "http://fit4life.com/fit4life/usersoap";
     private UserRepository userRepository;
+    private StudioRepository studioRepository;
     @PayloadRoot(namespace = NAMESPACE_URI, localPart="BanUserRequest")
     @ResponsePayload
     public BanUserResponse handleBanUser(@RequestPayload BanUserRequest request){
@@ -37,8 +43,6 @@ public class UserEndpoint {
                 response.setMessage("Ban flag was false - no action taken.");
             }
         }
-        
-        
         return response;
     }
 
@@ -80,6 +84,37 @@ public class UserEndpoint {
         } else{
             response.setStatus("NO_ACTION");
             response.setMessage("User not found!");
+        }
+        return response;
+    }
+
+    @PayloadRoot(namespace=NAMESPACE_URI, localPart="SubscribeStudioRequest")
+    @ResponsePayload
+    public SubscribeStudioResponse handleSubscribeStudio(@RequestPayload SubscribeStudioRequest request) {
+        SubscribeStudioResponse response = new SubscribeStudioResponse();
+
+        Optional<User> userOpt = userRepository.findByUsername(request.getUsername());
+        Optional<Studio> studioOpt = studioRepository.findById(request.getStudioId());
+
+        if(userOpt.isEmpty()){
+            response.setStatus("NO_ACTION");
+            response.setMessage("User not found!");
+        } else if (studioOpt.isEmpty()){
+            response.setStatus("NO_ACTION");
+            response.setMessage("Studio not found!");
+        } else{
+            User user = userOpt.get();
+            Studio studio = studioOpt.get();
+            Subscription sub = new Subscription();
+
+            sub.setStudio(studio);
+            sub.setUser(user);
+
+            user.getSubscriptions().add(sub);
+            userRepository.save(user);
+
+            response.setStatus("SUCCESS");
+            response.setMessage("User subscribed to studio successfully!");
         }
         return response;
     }
